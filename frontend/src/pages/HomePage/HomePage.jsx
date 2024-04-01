5b656abf67e2c281615dec807ea07d1c17533919
Kenny Lam <lamkenny1010@gmail.com>
2024-04-01 15:24:43 +1300

    PPENG 16: Very basic home page layout.

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import FilterPanel from "../../components/FilterPanel/FilterPanel";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import SortBy from "../../components/SortBy/SortBy";
+import styles from "./HomePage.module.css";
+import FoodList from "../../components/FoodList/FoodList";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className={styles.home_container}>
+      <div className={styles.container_one}>
+        <FilterPanel />
+      </div>
+      <div className={styles.container_two}>
+        <div className={styles.feature_container}>
+          <div className={styles.search_bar}>
+            <SearchBar />
+          </div>
+          <div className={styles.sort_by}>
+            <SortBy />
+          </div>
+        </div>
+        <div className={styles.food_list}>
+          <FoodList />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
