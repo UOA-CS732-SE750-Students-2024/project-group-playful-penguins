8d30f5ca2f21f4c3524d5705616c0701f97f6afb
Kenny Lam <lamkenny1010@gmail.com>
2024-04-02 10:52:29 +1300

    PPENG 16: Added input field.

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <div>
+      <input
+        type="text"
+        id="searchFood"
+        placeholder="Search for food.."
+        title="Type in food name"
+      />
+    </div>
+  );
 }
