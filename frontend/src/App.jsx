a4e35ebc9d8390e5aa0b46cd8a1ddfe5b38c5e70
Bmcl755 <blairmclean210@gmail.com>
2024-04-04 01:25:38 +1300

    Created simple static expanded card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import ExpandedTakeoutCard from './ExpandedTakeoutCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        
+        <ExpandedTakeoutCard/>
       </div>
      
     </>
