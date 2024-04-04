f16c304641484a6f98a49cd18fdfeda0fe0e735d
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2024-04-05 11:23:44 +1300

    PPENG-17 Refactor to use SVG icons instead of png

--- frontend/src/Views/LandingPage.jsx
+++ frontend/src/Views/LandingPage.jsx
@@ -1,51 +1,54 @@
 import styles from "./LandingPage.module.css";
-
+import UserIcon from "../assets/SVGIconComponents/UserIcon";
 
 export default function LandingPage() {
-    console.log(styles);
+  console.log(styles);
   return (
     <>
       <div className={styles.topbar}>
         <img
-          id={styles['logo-icon']}
+          id={styles["logo-icon"]}
           src="/images/app-logo.png"
           width="80px"
           height="80px"
           alt="logo for playful penguins"
         />
-        <img
-          id={styles['user-icon']}
-          src="/images/user.png"
-          width="25px"
-          height="25px"
-          alt="user icon"
-        />
+        <div id={styles["user-icon"]}>
+          <UserIcon />
+        </div>
       </div>
 
-      <div className={styles['slogan-container']}>
-        <h1 className={`${styles['quicksand-regular']} ${styles['slogan-first-line']}`}>
+      <div className={styles["slogan-container"]}>
+        <h1
+          className={`${styles["quicksand-regular"]} ${styles["slogan-first-line"]}`}
+        >
           Can't decide what to eat? no worries
           <br />
-          <b className={`${styles['quicksand-bold']} ${styles['slogan-second-line']}`}>
+          <b
+            className={`${styles["quicksand-bold"]} ${styles["slogan-second-line"]}`}
+          >
             PLAYFUL PENGUINS GOT YOU COVERED
           </b>
         </h1>
       </div>
 
-      <div className={styles['CTAs-container']}>
-        <div className={`${styles['CTA']} ${styles['CTA-takeout']}`}>
-          <h3 className={styles['quicksand-medium']}>
-            Don't feel like cooking, we got you&nbsp; 
-            <strong className={styles['quicksand-bold']}>
-               many healthy takeout options to choose from your loved brands
+      <div className={styles["CTAs-container"]}>
+        <div className={`${styles["CTA"]} ${styles["CTA-takeout"]}`}>
+          <h3 className={styles["quicksand-medium"]}>
+            Don't feel like cooking, we got you&nbsp;
+            <strong className={styles["quicksand-bold"]}>
+              many healthy takeout options to choose from your loved brands
             </strong>
           </h3>
-          <button id={`${styles['CTA-button-takeout']}`} className={styles["quicksand-bold"]}>
+          <button
+            id={`${styles["CTA-button-takeout"]}`}
+            className={styles["quicksand-bold"]}
+          >
             Takeout
           </button>
         </div>
-        <div className={`${styles['CTA']} ${styles['CTA-cook']}`}>
-          <h3 className={styles['quicksand-bold']}>
+        <div className={`${styles["CTA"]} ${styles["CTA-cook"]}`}>
+          <h3 className={styles["quicksand-bold"]}>
             You got some saute skills,&nbsp;
             <span className={styles["quicksand-medium"]}>
               playful penguins <br />
@@ -53,12 +56,14 @@ export default function LandingPage() {
               test !
             </span>
           </h3>
-          <button id={`${styles['CTA-button-cook']}`} className={styles["quicksand-bold"]}>
-            Cook at home 
+          <button
+            id={`${styles["CTA-button-cook"]}`}
+            className={styles["quicksand-bold"]}
+          >
+            Cook at home
           </button>
         </div>
-      </div>      
+      </div>
     </>
-    
   );
 }
