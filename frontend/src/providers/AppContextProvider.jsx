dbeaa99fac95da2d2afd08ddf5eb33172fcec40f
Kenny Lam <lamkenny1010@gmail.com>
2024-04-10 19:10:43 +1200

    PPENG 24: Updated AppContextProvider.

--- frontend/src/providers/AppContextProvider.jsx
+++ frontend/src/providers/AppContextProvider.jsx
@@ -5,9 +5,16 @@ export const AppContext = React.createContext({});
 export function AppContextProvider({ children }) {
   const [isTakeout, setTakeout] = useState(true);
   const changeCategory = (boolean) => setTakeout(boolean);
-
   const [calorieCountValuesFilter, setCalorieCountValuesFilter] = useState([
-    0, 100,
+    20, 50,
+  ]);
+  const [prepTimeValuesFilter, setPrepTimeValuesFilter] = useState([20, 50]);
+  const [cookingTimeValuesFilter, setCookingTimeValuesFilter] = useState([
+    20, 50,
+  ]);
+  const [priceValuesFilter, setPriceValuesFilter] = useState([20, 50]);
+  const [deliveryTimeValuesFilter, setDeliveryTimeValuesFilter] = useState([
+    20, 50,
   ]);
 
   const context = {
@@ -15,6 +22,14 @@ export function AppContextProvider({ children }) {
     changeCategory,
     calorieCountValuesFilter,
     setCalorieCountValuesFilter,
+    prepTimeValuesFilter,
+    setPrepTimeValuesFilter,
+    cookingTimeValuesFilter,
+    setCookingTimeValuesFilter,
+    priceValuesFilter,
+    setPriceValuesFilter,
+    deliveryTimeValuesFilter,
+    setDeliveryTimeValuesFilter,
   };
 
   return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
