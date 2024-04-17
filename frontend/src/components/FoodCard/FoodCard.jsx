8eb5c83dfa031b1cbedc0ec548c4e2664a9a5977
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2024-04-18 10:39:58 +1200

    PPENG-26 created the card component

--- frontend/src/components/FoodCard/FoodCard.jsx
+++ frontend/src/components/FoodCard/FoodCard.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import styles from './FoodCard.module.css';
+
+const title = "Burrito bowl with chipotle black beans";
+const image = "../../public/images/bowl.png"; 
+const time = "35";
+const serves = "2";
 
 export function FoodCard() {
-  return <div>FoodCard</div>;
+
+  return (
+    <Card className={styles.card}>
+      <CardMedia
+        component="img"
+        image={image}
+        alt={title}
+        className={styles.cardMedia}
+      />
+      <CardContent className={styles.cardContent}>
+        <Typography gutterBottom component="div" className={styles.title}>
+          {title}
+        </Typography>
+        <div className={styles.details}>
+          <Typography variant="body2" color="text.secondary">
+            {time} mins
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Serves {serves}
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
