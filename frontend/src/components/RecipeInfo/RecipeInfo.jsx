8c03d4ce490237730500eda84c2c1afda1d8d3cd
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2024-04-22 11:42:28 +1200

    PPENG-25 Used MUI Grid for the displaying the directions

--- frontend/src/components/RecipeInfo/RecipeInfo.jsx
+++ frontend/src/components/RecipeInfo/RecipeInfo.jsx
@@ -7,6 +7,8 @@ import Box from "@mui/system/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import { Grid, styled } from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 function generate(element) {
   return [0, 1, 2].map((value) =>
@@ -15,6 +17,69 @@ function generate(element) {
     })
   );
 }
+
+const StyledListItem = styled(ListItem)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "flex-start",
+}));
+
+const StepNumber = styled(ListItemText)(({ theme }) => ({
+  minWidth: "50px",
+  fontWeight: "bold",
+  marginRight: theme.spacing(2), // space between step number and text
+}));
+
+const StepContainer = styled(Grid)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  padding: theme.spacing(1, 0), // Apply vertical padding
+}));
+
+
+const StepLabel = styled('div')({
+  fontWeight: 'bold',
+  marginRight: '8px', // Adjust spacing as needed
+});
+
+const DirectionText = styled('div')({});
+
+
+
+const recipeName = "Veggie Yaki Udon";
+const numServings = "2";
+const prepTime = "25 mins";
+const cookingTime = "20 mins";
+const recipeDescription =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Facilis eum similique delectus quia minus quaerat cum quibusdamcupiditate. Consequatur corporis mollitia nisi similique? Maximeporro doloremque,voluptatum sapiente, facere velit adipisciipsam optio praesentium perspiciatis, ad quo vitae namreprehenderit?";
+const courseType = ["Dinner", "Lunch"];
+const cuisineType = "Japanese";
+const dietRequirementLabel = "VEGAN";
+const ingredientList = [
+  "2 packs of udon noodles",
+  "2 tablespoons sesame oil",
+  "1 onion, sliced",
+  "1 bell pepper, sliced",
+  "1 cup mushrooms, sliced",
+  "2 tablespoons soy sauce",
+  "1 cup shredded cabbage",
+];
+const directions = [
+  "Prepare the udon noodles according to the package instructions, usually boiling them in water for 3-4 minutes, then drain and set aside.",
+  "Heat the sesame oil in a large pan or wok over medium-high heat.",
+  "Add the sliced onion and bell pepper to the pan. Sauté for about 5 minutes or until the vegetables start to soften.",
+  "Add the mushrooms to the pan and continue to cook for another 3-4 minutes, until all the vegetables are tender and nicely browned.",
+  "Toss in the cooked udon noodles along with the soy sauce. Stir well to combine and ensure the noodles are well coated with the sauce.",
+  "Stir in the shredded cabbage and cook for another 2 minutes, just until the cabbage is slightly wilted but still crunchy.",
+  "Serve hot, garnished with sesame seeds or chopped green onions if desired.",
+];
+
+const nutritionInfo = [
+  { type: "Calories Content", amount: `100 kcal` },
+  { type: "Protein Content", amount: `12.5g` },
+  { type: "Carbohydrates Content", amount: `11.4g` },
+  { type: "Total Fat Content", amount: `1.3g` },
+];
 
 export default function RecipeInfo() {
   return (
@@ -33,49 +98,46 @@ export default function RecipeInfo() {
             </Box>
             <Box className={styles["basic-info"]}>
               {/* {Recipe name} */}
-              <h1 className={fontStyle["quicksand-bold"]}>Veggie Yaki Udon </h1>
+              <h1 className={fontStyle["quicksand-bold"]}>{recipeName}</h1>
               {/* Timing information */}
               <div className={styles["time-info"]}>
                 <div id={styles["clock-icon"]}>
                   <ClockIcon />
                 </div>
                 <h3 className={fontStyle["quicksand-medium"]}>
-                  Prep time 25 mins
+                  Prep time {prepTime}
                 </h3>
                 <div id={styles["clock-icon"]}>
                   <ClockIcon />
                 </div>
                 <h3 className={fontStyle["quicksand-medium"]}>
-                  Prep time 25 mins
+                  Prep time {cookingTime}
                 </h3>
               </div>
               <div className={styles["servings-info"]}>
                 <div id={styles["servings-icon"]}>
                   <ServingsIcon />
                 </div>
-                <h3 className={fontStyle["quicksand-medium"]}>2 servings</h3>
+                <h3 className={fontStyle["quicksand-medium"]}>
+                  {numServings} Servings
+                </h3>
               </div>
               <div className={fontStyle["quicksand-regular"]}>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Facilis eum similique delectus quia minus quaerat cum quibusdam
-                cupiditate. Consequatur corporis mollitia nisi similique? Maxime
-                porro doloremque, voluptatum sapiente, facere velit adipisci
-                ipsam optio praesentium perspiciatis, ad quo vitae nam
-                reprehenderit?
+                {recipeDescription}
               </div>
             </Box>
           </Box>
           <Box className={styles["food-type-container"]}>
             <h3 className={fontStyle["quicksand-regular"]}>
-              <strong>Course : </strong> Dinner, Lunch
+              <strong>Course : </strong> {`${courseType[0]}, ${courseType[1]}`}
             </h3>
             <h3 className={fontStyle["quicksand-regular"]}>
-              <strong>Cuisine : </strong> Japanese
+              <strong>Cuisine : </strong> {cuisineType}
             </h3>
             <Box
               className={`${fontStyle["quicksand-regular"]} ${styles["diet-label"]}`}
             >
-              VEGAN
+              {dietRequirementLabel}
             </Box>
           </Box>
           <Box className={styles["section-container"]}>
@@ -85,14 +147,17 @@ export default function RecipeInfo() {
                 className={`${fontStyle["quicksand-regular"]} ${styles["ingredients-list-container"]}`}
               >
                 <List>
-                  {generate(
-                    <ListItem>
+                  {ingredientList.map((ingredient, index) => (
+                    <ListItem key={index} className={styles["ingredient-item"]}>
                       <ListItemText
-                        className={`${fontStyle["quicksand-regular"]} ${styles["ingredient-item"]}`}
-                        primary="Ingredient name"
+                        primary={
+                          <Typography variant="body1">
+                            • {ingredient}
+                          </Typography>
+                        }
                       />
                     </ListItem>
-                  )}
+                  ))}
                 </List>
               </Box>
             </Box>
@@ -100,26 +165,39 @@ export default function RecipeInfo() {
               className={`${fontStyle["quicksand-semibold"]} ${styles["nutrients-container"]}`}
             >
               <h3>NUTRITION INFORMATION</h3>
-              <Box
-                className={`${fontStyle["quicksand-medium"]} ${styles["nutrients-item"]}`}
-              >
-                will show carbs
-              </Box>
+              {nutritionInfo.map((nutrient, index) => (
+                <Box
+                  key={index}
+                  className={`${fontStyle["quicksand-medium"]} ${styles["nutrients-item"]}`}
+                >
+                  {`${nutrient.type}:   ${nutrient.amount}`}
+                </Box>
+              ))}
             </Box>
           </Box>
           <Box className={styles["directions-container"]}>
-            <h2 className={fontStyle["quicksand-bold"]}>Directions</h2>
-            <List>
-                  {generate(
-                    <ListItem>
-                      <ListItemText
-                        className={`${fontStyle["quicksand-regular"]} ${styles["ingredient-item"]}`}
-                        primary="Step number"
-                        // secondary="Full description of the direction"
-                      />
-                    </ListItem>
-                  )}
-                </List>
+            <h2 className={`${fontStyle["quicksand-bold"]} ${styles["green-font"]}`}>Directions</h2>
+
+            {/* {EXPERIMENTING with grid } */}
+            <Grid container spacing={2}>
+              {directions.map((text, index) => (
+                <Grid
+                  key={index}
+                  container
+                  item
+                  xs={12}
+                  spacing={2}
+                  alignItems="center"
+                >
+                  <Grid item className={`${fontStyle["quicksand-bold"]} ${styles["direction-step-item"]}  ${styles["green-font"]}`}>
+                    <StepLabel>{`Step ${index + 1}`}</StepLabel>
+                  </Grid>
+                  <Grid item xs className={`${fontStyle["quicksand-bold"]} ${styles["direction-item"]}` }>
+                    <DirectionText>{text}</DirectionText>
+                  </Grid>
+                </Grid>
+              ))}
+            </Grid>
           </Box>
         </Box>
       </Box>
