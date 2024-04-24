36730176511dd2644e8e4f9230de0bb53f64ee3c
Kenny Lam <lamkenny1010@gmail.com>
2024-04-24 19:35:13 +1200

    PPENG 28: Basic layout for SortBy dropdown menu.

--- frontend/src/components/SortBy/SortBy.jsx
+++ frontend/src/components/SortBy/SortBy.jsx
@@ -1,5 +1,74 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Menu, MenuItem, Button, Typography, Box } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { colors } from "../../constants/styles-constant";
+import { AppContext } from "../../providers/AppContextProvider";
 
 export function SortBy() {
-  return <div>SortBy</div>;
+  const { isTakeout } = useContext(AppContext);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedSortByOption, setSelectedSortByOption] = useState("Sort By");
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleRequirementSelect = (requirement) => {
+    setSelectedSortByOption(requirement);
+    handleClose();
+  };
+
+  return (
+    <Box
+      sx={{
+        pt: "32px",
+        pl: "16px",
+        pr: "32px",
+        display: "flex",
+        justifyContent: "flex-end",
+      }}
+    >
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        sx={{
+          width: "150px",
+          color: "white",
+          backgroundColor: isTakeout ? "#77595E" : "#00665E",
+          borderRadius: "15px",
+          border: "1px solid #E0E0E0",
+          height: "65px",
+          justifyContent: "space-evenly",
+          fontSize: "16px",
+          textTransform: "none",
+          "&:hover": {
+            backgroundColor: isTakeout ? "#473538" : "#00665E",
+          },
+        }}
+        endIcon={<KeyboardArrowDownIcon />}
+      >
+        {selectedSortByOption}
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={() => handleRequirementSelect("Name(A-Z)")}>
+          Name(A-Z)
+        </MenuItem>
+        <MenuItem onClick={() => handleRequirementSelect("Name(Z-A)")}>
+          Name(Z-A)
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
 }
