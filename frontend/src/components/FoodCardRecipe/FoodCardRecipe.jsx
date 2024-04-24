7c9fb78f3e4bde0cd8b0332f9fdc6d5bedc95222
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2024-04-25 10:32:17 +1200

    created the card list view

--- frontend/src/components/FoodCardRecipe/FoodCardRecipe.jsx
+++ frontend/src/components/FoodCardRecipe/FoodCardRecipe.jsx
@@ -4,9 +4,11 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import styles from './FoodCardRecipe.module.css';
+import ClockIcon from "../../assets/SVGIconComponents/ClockIcon";
+import ServingsIcon from "../../assets/SVGIconComponents/ServingsIcon";
 
 const title = "Burrito bowl with chipotle black beans";
-const image = "../../public/images/bowl.png"; 
+const image = "../../public/images/bowl.png";
 const time = "35";
 const serves = "2";
 
--- frontend/src/components/FoodList/FoodList.jsx
+++ frontend/src/components/FoodList/FoodList.jsx
@@ -1,14 +1,23 @@
 import React, { useContext } from "react";
+import { Grid } from '@mui/material';
 import { FoodCardRecipe } from "../FoodCardRecipe/FoodCardRecipe";
 import { FoodCardTakeout } from "../FoodCardTakeout/FoodCardTakeout";
 import { AppContext } from "../../providers/AppContextProvider";
+import styles from './FoodList.module.css';
 
 export function FoodList() {
   const { isTakeout } = useContext(AppContext);
+  const cards = Array.from({ length: 10 }, (_, i) => i);
 
-  return (<div>
-    {isTakeout ? (
-      <FoodCardTakeout />
-    ) : (<FoodCardRecipe />)}</div>
+  return (
+    <div className={styles.scrollableContainer}>
+    <Grid container rowSpacing={10} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
+      {cards.map((card, index) => (
+        <Grid item xs={12} sm={6} md={4} key={index}>
+          {isTakeout ? <FoodCardTakeout /> : <FoodCardRecipe />}
+        </Grid>
+      ))}
+    </Grid>
+  </div>
   );
 }
