fedaf0727601de7dd3baa542de899ef122f0583f
rahalm22 <rahalm22@gmail.com>
2024-04-25 16:04:18 +1200

    PPENG-29 Implemented services on frontend

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -4,8 +4,26 @@ import { SearchBar } from "../../components/SearchBar/SearchBar";
 import { SortBy } from "../../components/SortBy/SortBy";
 import { FoodList } from "../../components/FoodList/FoodList";
 import styles from "./HomePage.module.css";
+import { useContext, useState } from "react";
+import RecipesService from "../../services/RecipeService";
 
 export function HomePage() {
+
+  const [recipeData, setRecipeData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchRecipeData = async () => {
+    setIsLoading(true);
+    try {
+      const data = await RecipesService.getRecipes();
+      setRecipeData(data);
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <div className={styles["home-container"]}>
       <div className={styles["filter-container"]}>
