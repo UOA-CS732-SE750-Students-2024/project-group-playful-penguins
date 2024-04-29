3842b4d707ab83b458d83c6bc8371aca2d86b559
Blair <bmcl755@aucklanduni.ac.nz>
2024-04-30 11:31:15 +1200

    PPENG-21 Fix close functionality of modal and add close button

--- frontend/src/components/FoodCardTakeout/FoodCardTakeout.jsx
+++ frontend/src/components/FoodCardTakeout/FoodCardTakeout.jsx
@@ -1,38 +1,59 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import styles from './FoodCardTakeout.module.css';
+import React, { useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import styles from "./FoodCardTakeout.module.css";
+import { ExpandedTakeoutCard } from "../../components/ExpandedTakeoutCard/ExpandedTakeoutCard";
+import { useState } from "react";
+import { Box } from "@mui/material";
 
 const title = "Garden Veggie Pita";
-const image = "../../public/images/pita.png"; 
+const image = "../../public/images/pita.png";
 const price = "";
 const restaurant = "Pita Pat";
 
 export function FoodCardTakeout() {
+  const [openTakeoutCard, setOpenTakeoutCard] = useState(false);
+
+  const handleOpen = () => {
+    setOpenTakeoutCard(true);
+  };
+  const handleClose = () => {
+    setOpenTakeoutCard(false);
+  };
 
   return (
-    <Card className={styles.card}>
-      <CardMedia
-        component="img"
-        image={image}
-        alt={title}
-        className={styles.cardMedia}
-      />
-      <CardContent className={styles.cardContent}>
-        <Typography gutterBottom component="div" className={styles.title}>
-          {title}
-        </Typography>
-        <div className={styles.details}>
-          <Typography variant="body2" color="text.secondary">
-            $$ {price} 
+    <Box>
+      <Card onClick={handleOpen} className={styles.card}>
+        <CardMedia
+          component="img"
+          image={image}
+          alt={title}
+          className={styles.cardMedia}
+        />
+        <CardContent className={styles.cardContent}>
+          <Typography gutterBottom component="div" className={styles.title}>
+            {title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
-            by {restaurant}
-          </Typography>
-        </div>
-      </CardContent>
-    </Card>
+          <div className={styles.details}>
+            <Typography variant="body2" color="text.secondary">
+              $$ {price}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              by {restaurant}
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+      <Modal
+        className={styles[`modal-container`]}
+        open={openTakeoutCard}
+        onClose={handleClose}
+      >
+        <ExpandedTakeoutCard handleClose={handleClose} />
+      </Modal>
+    </Box>
   );
 }
