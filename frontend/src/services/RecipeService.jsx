4335988fe78a9ca5813e4cd60525ce475accb049
Kenny Lam <lamkenny1010@gmail.com>
2024-04-30 00:40:13 +1200

    PPENG 35: Slider filters have integrated with the backend.

--- frontend/src/services/RecipeService.jsx
+++ frontend/src/services/RecipeService.jsx
@@ -2,15 +2,27 @@ import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-const getRecipes = async () => {
+const getRecipes = async (filters) => {
   try {
     const response = await axios.get(BACKEND_URL + "/recipes/");
     if (!response.data) {
       throw new Error("No data from backend");
     }
-    return response.data;
+    const recipes = response.data;
+    console.log(recipes);
+    const filteredRecipes = recipes.filter((recipe) => {
+      return (
+        // TODO Filter feature is implemented, but preparation time, calorie count and cooking time are supported
+        //      by the database because it does not have this value.
+        filters.prepTimeValues[0] <= recipe.pricePerServing &&
+        recipe.pricePerServing <= filters.prepTimeValues[1]
+      );
+    });
+    console.log(filteredRecipes);
+    return filteredRecipes;
   } catch (error) {
     console.error("Error fetching data: ", error);
+    return [];
   }
 };
 
