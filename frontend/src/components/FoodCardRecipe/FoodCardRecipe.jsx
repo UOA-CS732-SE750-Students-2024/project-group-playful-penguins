78e0609cebeb8bff5e8a77185808bff12d228902
Sukhleen Kaur <skau811@aucklanduni.ac.nz>
2024-04-29 23:03:09 +1200

    PPENG-33 made changes to address review feedback

--- frontend/src/components/FoodCardRecipe/FoodCardRecipe.jsx
+++ frontend/src/components/FoodCardRecipe/FoodCardRecipe.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import styles from './FoodCardRecipe.module.css';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import styles from "./FoodCardRecipe.module.css";
 import { useNavigate } from "react-router-dom";
-import Skeleton from 'react-loading-skeleton';
 
 const title = "Burrito bowl with chipotle black beans";
-const image = "../../public/images/bowl.png"; 
+const image = "../../public/images/bowl.png";
 const time = "35";
 const serves = "2";
 
-export function FoodCardRecipe({data}) {
+export function FoodCardRecipe({ data }) {
   const navigate = useNavigate();
 
-  function openRecipeInfo(){
-    console.log("clicked");
+  function openRecipeInfo() {
     navigate(`recipe/${data.id}`);
-    
   }
   return (
     <Card className={styles.card} onClick={openRecipeInfo}>
@@ -30,11 +27,11 @@ export function FoodCardRecipe({data}) {
       />
       <CardContent className={styles.cardContent}>
         <Typography gutterBottom component="div" className={styles.title}>
-          {data.title || Skeleton}
+          {data.title}
         </Typography>
         <div className={styles.details}>
           <Typography variant="body2" color="text.secondary">
-            {data.readyInMinutes || Skeleton} mins
+            {data.readyInMinutes} mins
           </Typography>
           <Typography variant="body2" color="text.secondary">
             Serves {data.servings}
