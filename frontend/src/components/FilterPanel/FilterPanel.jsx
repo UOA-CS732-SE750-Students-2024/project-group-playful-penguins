3e5d35eeef6ca69c2bd09ce0b0b5e254db928a26
Kenny Lam <lamkenny1010@gmail.com>
2024-04-30 09:46:19 +1200

    PPENG 35: Create function that make apply filters.

--- frontend/src/components/FilterPanel/FilterPanel.jsx
+++ frontend/src/components/FilterPanel/FilterPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../providers/AppContextProvider";
 import { CalorieCountFilter } from "../Filters/CalorieCountFilter/CalorieCountFilter";
 import { PrepTimeFilter } from "../Filters/PrepTimeFilter/PrepTimeFilter";
@@ -6,89 +6,124 @@ import { DietRequirementFilter } from "../Filters/DietRequirementFilter/DietRequ
 import { CookingTimeFilter } from "../Filters/CookingTimeFilter/CookingTimeFilter";
 import { DeliveryTimeFilter } from "../Filters/DeliveryTimeFilter/DeliveryTimeFilter";
 import { PriceFilter } from "../Filters/PriceFilter/PriceFilter";
-import { Box, Button } from "@mui/material";
+import { Box, Button, CircularProgress } from "@mui/material";
 import { colors, FILTERS } from "../../constants/styles-constant";
+import RecipesService from "../../services/RecipeService";
+import { LoadingButton } from "@mui/lab";
 
 export function FilterPanel() {
-  const { isTakeout, setFilters } = useContext(AppContext);
+  const { isTakeout, filters, setFilters } = useContext(AppContext);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleApplyFilters = async (event) => {
+    // Prevent default form submission
+    event.preventDefault();
+
+    setIsLoading(true);
+    try {
+      const data = await RecipesService.getFilteredRecipes(filters);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching filtered recipes:", error);
+    }
+    setIsLoading(false);
+  };
 
   const handleReset = () => {
-    // Create a new object to hold the reset filters
     const resetFilters = {};
-
     // Iterate over the keys in FILTERS
     Object.keys(FILTERS).forEach((key) => {
       // Assign the initial value from FILTERS to the corresponding state key
       resetFilters[FILTERS[key].STATE_KEY] = FILTERS[key].INITIAL_VALUE;
     });
-
     setFilters(resetFilters);
   };
 
   return (
-    <Box
-      sx={{
-        pt: "16px",
-      }}
-    >
-      <CalorieCountFilter />
-      {isTakeout ? (
-        <div>
-          <PriceFilter />
-          <DeliveryTimeFilter />
-        </div>
-      ) : (
-        <div>
-          <PrepTimeFilter />
-          <CookingTimeFilter />
-        </div>
-      )}
-      <DietRequirementFilter />
+    <form onSubmit={handleApplyFilters}>
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          pl: "16px",
-          pr: "16px",
-          pt: "8px",
-          margin: "auto",
+          pt: "16px",
         }}
       >
-        <Button
-          variant="text"
-          size="large"
+        <CalorieCountFilter />
+        {isTakeout ? (
+          <div>
+            <PriceFilter />
+            <DeliveryTimeFilter />
+          </div>
+        ) : (
+          <div>
+            <PrepTimeFilter />
+            <CookingTimeFilter />
+          </div>
+        )}
+        <DietRequirementFilter />
+        <Box
           sx={{
-            flex: 1, // TODO change color dynamically
-            color: "white",
-            borderRadius: "15px",
-            backgroundColor: isTakeout ? "#EDB1BB" : "#00CCBB",
-            "&:hover": {
-              backgroundColor: isTakeout ? "#473538" : "#00665E",
-            },
-            marginRight: "8px",
+            display: "flex",
+            justifyContent: "space-between",
+            pl: "16px",
+            pr: "16px",
+            pt: "8px",
+            margin: "auto",
           }}
         >
-          Apply
-        </Button>
-        <Button
-          onClick={handleReset}
-          variant="text"
-          size="large"
-          sx={{
-            flex: 1,
-            color: "black",
-            borderRadius: "15px",
-            backgroundColor: "#D9D9D9",
-            "&:hover": {
-              backgroundColor: "darkgray",
-              borderColor: "darkgray",
-            },
-            marginLeft: "8px",
-          }}
-        >
-          Reset
-        </Button>
+          {isLoading ? (
+            <LoadingButton
+              loading={isLoading}
+              size="large"
+              sx={{
+                flex: 1,
+                borderRadius: "15px",
+                marginRight: "8px",
+                backgroundColor: "transparent",
+                border: isTakeout ? "2px solid #EDB1BB" : "2px solid #00CCBB",
+                "& .MuiCircularProgress-colorPrimary": {
+                  color: isTakeout ? "#EDB1BB" : "#00CCBB",
+                },
+              }}
+              loadingIndicator={<CircularProgress size={24} />} // Custom circular progress indicator
+            />
+          ) : (
+            <Button
+              type="submit"
+              variant="text"
+              size="large"
+              sx={{
+                flex: 1,
+                color: "white",
+                borderRadius: "15px",
+                backgroundColor: isTakeout ? "#EDB1BB" : "#00CCBB",
+                "&:hover": {
+                  backgroundColor: isTakeout ? "#473538" : "#00665E",
+                },
+                marginRight: "8px",
+              }}
+            >
+              Apply
+            </Button>
+          )}
+          <Button
+            onClick={handleReset}
+            variant="text"
+            size="large"
+            sx={{
+              flex: 1,
+              color: "black",
+              borderRadius: "15px",
+              backgroundColor: "#D9D9D9",
+              "&:hover": {
+                backgroundColor: "darkgray",
+                borderColor: "darkgray",
+              },
+              marginLeft: "8px",
+            }}
+          >
+            Reset
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </form>
   );
 }
