fbae93879f14cab34c535b253e18eda3ceac38c6
rahalm22 <rahalm22@gmail.com>
2024-04-30 14:00:36 +1200

    PPENG-31 Resolved Conflicts

--- frontend/src/components/FoodList/FoodList.jsx
+++ frontend/src/components/FoodList/FoodList.jsx
@@ -1,22 +1,30 @@
 import React, { useContext } from "react";
-import { Grid } from '@mui/material';
+import { Grid } from "@mui/material";
 import { FoodCardRecipe } from "../FoodCardRecipe/FoodCardRecipe";
 import { FoodCardTakeout } from "../FoodCardTakeout/FoodCardTakeout";
 import { useNavigate } from "react-router-dom";
 import { AppContext } from "../../providers/AppContextProvider";
-import styles from './FoodList.module.css';
+import styles from "./FoodList.module.css";
 
-
-export function FoodList({foodData}) {
-  const { isTakeout } = useContext(AppContext);return (
+export function FoodList({ foodData }) {
+  const { isTakeout } = useContext(AppContext);
+  return (
     <div className={styles.scrollableContainer}>
-    <Grid container rowSpacing={10} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
-      {foodData.map((item, index) => (
-        <Grid item xs={12} sm={6} md={4} key={index}>
-          {isTakeout ? <FoodCardTakeout key={index} data={item}/> : <FoodCardRecipe key={index} data={item}/>}
+      {foodData && foodData.length > 0 ? (
+        <Grid container rowSpacing={10} columnSpacing={{ xs: 1, sm: 2, md: 4 }}>
+          {foodData.map((item, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              {isTakeout ? (
+                <FoodCardTakeout key={index} data={item} />
+              ) : (
+                <FoodCardRecipe key={index} data={item} />
+              )}
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
-  </div>
+      ) : (
+        <p>No data available.</p>
+      )}
+    </div>
   );
 }
