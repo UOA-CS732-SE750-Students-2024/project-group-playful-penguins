15f2a3729d286a2b6c0270a6542412161fa4abcd
Imashi Kinigama <78595183+timelesswanderder24@users.noreply.github.com>
2024-05-07 11:19:06 +1200

    ppeng-58 changes to the recipe card hearts

--- frontend/src/components/FoodCardRecipe/FoodCardRecipe.jsx
+++ frontend/src/components/FoodCardRecipe/FoodCardRecipe.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import styles from "./FoodCardRecipe.module.css";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import LocalDiningIcon from "@mui/icons-material/LocalDining";
 import { useNavigate } from "react-router-dom";
@@ -11,6 +14,7 @@ import { Box } from "@mui/material";
 
 export function FoodCardRecipe({ data }) {
   const navigate = useNavigate();
+  const [favorite, setFavorite] = useState(false);
 
   function openRecipeInfo() {
     navigate(`recipe/${data.id}`);
@@ -18,6 +22,9 @@ export function FoodCardRecipe({ data }) {
 
   // Truncate title if it's longer than 60 characters
   const displayTitle = data.title.length > 60 ? `${data.title.substring(0, 40)}...` : data.title;
+  const toggleFavorite = () => {
+    setFavorite(!favorite);
+  };
 
   return (
     <Card className={styles.card} onClick={openRecipeInfo}>
@@ -42,6 +49,16 @@ export function FoodCardRecipe({ data }) {
           <Typography variant="body2" color="text.secondary">
             Serves {data.servings}
           </Typography>
+          <IconButton
+        onClick={(event) => {
+          event.stopPropagation();
+          toggleFavorite();
+        }}
+        className={styles.favoriteIcon}
+        aria-label="add to favorites"
+      >
+        {favorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+      </IconButton>
         </div>
       </CardContent>
     </Card>
