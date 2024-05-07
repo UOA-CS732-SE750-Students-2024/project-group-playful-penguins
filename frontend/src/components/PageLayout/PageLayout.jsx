83d6ed56fcfda25f93bc5a3aaf32a766a3a53de9
Kenny Lam <lamkenny1010@gmail.com>
2024-05-07 17:06:31 +1200

    PPENG 65: Remove footer.

--- frontend/src/components/PageLayout/PageLayout.jsx
+++ frontend/src/components/PageLayout/PageLayout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { Navbar } from "../Navbar/Navbar";
-import { Footer } from "../Footer/Footer";
 
 export function PageLayout() {
   return (
@@ -10,7 +9,6 @@ export function PageLayout() {
       <div>
         <Outlet />
       </div>
-      <Footer />
     </React.Fragment>
   );
 }
